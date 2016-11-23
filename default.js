aaee14f6e601fde9a79776a93631cc3b3f07522e
Vishnu <vishnu_venugopal@live.com>
2016-11-23 14:26:51 -0800

    Added html structure #1

---
+++ default.js
@@ -0,0 +1,24 @@
+var profile = {
+
+    profilePicture: 'https://pbs.twimg.com/profile_images/785502482478735360/WyoqOzXi.jpg',
+    about: 'I act. I tell jokes. I try to sing and dance. Spread love. Spread light. Go Blue.',
+    updates: [newUpdate('12:30', 'Got me working through lunch!'),
+              newUpdate('12:27', 'Man this coding thing is tough'),
+              newUpdate('9:25', 'Taco Bell for breakfast, but I am at class now'),
+              newUpdate('8:55', 'On the way to drop of these glasses.'),
+              newUpdate('8:45', 'Mom left her glasses, gotta get those to her.')
+             ]
+}
+var coverPhoto = {
+  cover: 'https://pbs.twimg.com/media/Cq2E4VfUkAEuJ6i.jpg'
+}
+
+function newUpdate(timestamp, post) {
+  return {timestamp: timestamp, post: post};
+}
+
+var mini-profile = {
+  mini-pic: 'https://pbs.twimg.com/profile_images/785502482478735360/WyoqOzXi.jpg'
+  username: Vishnu Venugopal
+  twittername: Vish2thenu
+}
