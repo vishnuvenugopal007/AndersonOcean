833a0a789efeed12a919774cbf809de9d221388b
Vishnu <vishnu_venugopal@live.com>
2016-11-23 16:01:04 -0800

    Submitting for reference #1

--- default.js
+++ default.js
@@ -1,6 +1,7 @@
-var profile = {
-
-    profilePicture: 'https://pbs.twimg.com/profile_images/785502482478735360/WyoqOzXi.jpg',
+var user = {
+    username: "Vishnu",
+    twitterHandle: "Vish2thenu",
+    profilePicture: "https://pbs.twimg.com/profile_images/785502482478735360/WyoqOzXi.jpg",
     about: 'I act. I tell jokes. I try to sing and dance. Spread love. Spread light. Go Blue.',
     updates: [newUpdate('12:30', 'Got me working through lunch!'),
               newUpdate('12:27', 'Man this coding thing is tough'),
@@ -17,8 +18,37 @@ function newUpdate(timestamp, post) {
   return {timestamp: timestamp, post: post};
 }
 
+function newElement(tagName, className, text) {
+  var newElement = document.createElement(tagName);
+  newElement.className = className;
+  if(text !== null)
+    newElement.appendChild(docuent.createTextNode(text));
+    return newElement;
+}
+
+function displayProfile() {
+  var point = document.getElementByID('photo');
+  point.style.backgroundImage = 'url(' + user.profilePicture + ')';
+  point = document.getElementById('twitterhandle');
+  point.appendChild(document.createTextNode('@' + user.twitterHandle));
+  point = document.getElementById('about');
+  point.appendChild(document.createTextNode(user.aboutMe));
+}
+
+function displayUpdates() {
+  var i;
+  var containerPoint = document.getElementByID('updates');
+  for (i=0; i < user.updates.length; i++) {
+    containerPoint.appendChild(newElement('div', 'update', null));
+    childPoint = contaierPoint.lastChild;
+    childPoint.appendChild(newElement('h4', 'username', '@' + user.twitterHandle))
+    childPoint.appendChild(newElement('p', 'timestamp', user.updates[i].timestamp))
+    childPoint.appendChild(newElement('p', 'post', user.updates[i].post))
+  }
+}
+
 var miniProfile = {
   miniPic: 'https://pbs.twimg.com/profile_images/785502482478735360/WyoqOzXi.jpg',
   Name: "Vishnu Venugopal",
-  twittername: "Vish2thenu"
+  twitterhandle: "Vish2thenu",
 }
