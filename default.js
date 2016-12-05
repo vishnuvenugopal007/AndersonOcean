b4acdbc79dc7381d9f9c239e35a26957af449a64
Vishnu <vishnu_venugopal@live.com>
2016-12-04 22:07:58 -0800

    Fixed header, improved implementation for CoverPhoto #5

--- default.js
+++ default.js
@@ -96,17 +96,21 @@ for (var i = 0; i < tweets.length; i++) {
   var $feed = document.getElementById('feed')
   $feed.appendChild($tweet)
 }
+
+/** Profile elements (name, handle, about & cover photo) */
 function renderProfile(profile) {
 
   var $profile = document.createElement('div')
   $profile.setAttribute('id', 'miniProfile')
 
+
   var $cover = document.createElement('img')
   $cover.setAttribute('id', 'canopy')
-  $cover.setAttribute('coverPhoto', profile.coverPhoto)
+  $cover.setAttribute('coverPhoto', profiles.coverPhoto)
+  $cover.src = profile.coverPhoto
 
   var $thumbnail = document.createElement('img')
-  $thumbnail.setAttribute('profilePic', profile.thumbnail)
+  $thumbnail.setAttribute('profilePic', profiles.thumbnail)
   $thumbnail.src = profile.thumbnail
 
   var $userInfo = document.createElement('div')
@@ -148,6 +152,7 @@ function renderProfile(profile) {
 var profiles = [
   {
     coverPhoto: 'https://pbs.twimg.com/media/Cq2E4VfUkAEuJ6i.jpg',
+    coverPosition: 'center top',
     thumbnail: 'https://pbs.twimg.com/profile_images/785502482478735360/WyoqOzXi_200x200.jpg',
     name: 'Vishnu',
     handle: 'Vish2thenu',
